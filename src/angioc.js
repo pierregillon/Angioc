d91e63129c67eb30da9e522b4c5a6f8b9e607cd9
Pierre GILLON <gillon.pierre69@gmail.com>
2015-08-29 11:50:27 +0200

    Add dependencies management and singleton.

--- src/angioc.js
+++ src/angioc.js
@@ -1,64 +1,84 @@
-function Angioc(){
-    var self = this;
-    var registry = {};
-
-    self.register = function(name, body){
-        if(typeof name !== 'string' || !name){
-            throw new Error('Unable to register element : the first argument should be a dependency name.');
-        }
-        if(!body){
-            throw new Error('Unable to register element : the second argument should be defined.');
-        }
-        return new SpecificationFactory(body, function(spec){
-            registry[name] = spec;
-        });
-    };
-
-    self.resolve = function(name){
-        if(registry.hasOwnProperty(name) === false){
-            throw new Error();
-        }
-        return registry[name].getInstance();
-    };
-}
-
-function SpecificationFactory(body, callback){
-    var self = this;
-
-    self.asClass = function(){
-        callback(new ClassSpecification(body));
-    };
-
-    self.asConstant = function(){
-
-    };
-}
-
-function ClassSpecification(body){
-    var self = this;
-    var instance = undefined;
-
-    self.getInstance = function(){
-        if(instance === undefined){
-            instance = createInstance();
-        }
-        return instance;
-    };
-
-    function createInstance(){
-        return new body();
-    }
-}
-
-(function(factory){
+(function(){
     'use strict';
+    function Angioc(){
+        var self = this;
+        var registry = {};
 
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory;
-    } else {
-        window.angioc = factory;
+        self.register = function(name, body){
+            if(typeof name !== 'string' || !name){
+                throw new Error('Unable to register element : the first argument should be a dependency name.');
+            }
+            if(!body){
+                throw new Error('Unable to register element : the second argument should be defined.');
+            }
+            return new SpecificationFactory(body, self.resolve, function(spec){
+                registry[name] = spec;
+            });
+        };
+
+        self.resolve = function(name){
+            if(registry.hasOwnProperty(name) === false){
+                throw new Error();
+            }
+            return registry[name].getInstance();
+        };
     }
 
-}(new Angioc()));
+    function SpecificationFactory(body, dependencyResolver, callback){
+        var self = this;
+
+        self.asClass = function(){
+            var spec = new ClassSpecification(body, dependencyResolver);
+            callback(spec);
+            return spec;
+        };
+
+        self.asConstant = function(){
+
+        };
+    }
+
+    function ClassSpecification(constructor, dependencyResolver){
+        var self = this;
+        var dependencyNames = [];
+        var isSingleton = false;
+        var instance = undefined;
+
+        self.getInstance = function(){
+            if(!isSingleton || !instance){
+                instance = createInstance();
+            }
+            return instance;
+        };
+
+        self.withDependencies = function(names){
+            dependencyNames = names;
+        };
+
+        self.asSingleton = function(){
+            isSingleton = true;
+        };
+
+        function createInstance(){
+            var parameterizedConstructor = constructor;
+            dependencyNames.forEach(function(dependencyName){
+                var dependency = dependencyResolver(dependencyName);
+                parameterizedConstructor = parameterizedConstructor.bind(this, dependency);
+            });
+            return new parameterizedConstructor();
+        }
+    }
+
+
+    function injectInstance(factory){
+        if (typeof define === 'function' && define.amd) {
+            define([], factory);
+        } else if (typeof exports === 'object') {
+            module.exports = factory;
+        } else {
+            window.angioc = factory;
+        }
+    }
+
+    injectInstance(new Angioc());
+}());
--- src/angioc.spec.js
+++ src/angioc.spec.js
@@ -36,9 +36,11 @@
             expect(window.angioc).toBeDefined();
         });
 
-        it('can resolve dependency name when class has been registered with this name.', function(){
+        it('can get class instance when resolving dependency name.', function(){
             // Actors
-            function MyClass(){}
+            function MyClass(){
+                this.say = function(message){};
+            }
 
             // Actions
             angioc
@@ -48,6 +50,54 @@
             // Asserts
             var instance = angioc.resolve('MyDependency');
             expect(instance).toBeDefined();
+            expect(instance.say).toBeDefined();
+        });
+
+        it('returns same class instance when resolving singleton dependency name.', function(){
+            // Actors
+            function MyClass(){
+                this.say = function(message){};
+            }
+
+            // Actions
+            angioc
+                .register('MyDependency', MyClass)
+                .asClass()
+                .asSingleton();
+
+            // Asserts
+            var instance1 = angioc.resolve('MyDependency');
+            var instance2 = angioc.resolve('MyDependency');
+            expect(instance1).toBe(instance2);
+        });
+
+        it('can get class instance with dependencies when resolving dependency name.', function(){
+            // Actors
+            function MyClass(myClass2){
+                this.get = function(){
+                    return 'MyClass' + myClass2.get();
+                };
+            }
+            function MyClass2(){
+                this.get = function(){
+                    return 'MyClass2';
+                };
+            }
+
+            // Actions
+            angioc
+                .register('MyClass2', MyClass2)
+                .asClass();
+            angioc
+                .register('MyClass', MyClass)
+                .asClass()
+                .withDependencies(['MyClass2']);
+
+            // Asserts
+            var instance = angioc.resolve('MyClass');
+            expect(instance).toBeDefined();
+            expect(instance.get).toBeDefined();
+            expect(instance.get()).toBe('MyClassMyClass2');
         });
     })
 }(window, describe, it));
